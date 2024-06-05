7cbc899c0b2d6f85ec0843b6e367c8a395cd9198
anjan <anjan@Anjana>
2024-06-05 11:42:27 +0530

    Access push

---
+++ sampleprogram/src/main/java/accessmodifier/Access2.java
@@ -0,0 +1,14 @@
+package accessmodifier;
+
+public class Access2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Access1 b=new Access1();
+b.display1();
+
+b.display3();
+b.display4();
+	}
+
+}
---
+++ sampleprogram/src/main/java/accessmodifier2/Modifier1.java
@@ -0,0 +1,16 @@
+package accessmodifier2;
+
+import accessmodifier.Access1;
+
+public class Modifier1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Access1 c=new Access1();
+		c.display1();
+		c.display3();
+		c.display4();
+
+	}
+
+}
